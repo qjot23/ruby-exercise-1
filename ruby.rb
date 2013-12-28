bb13ed22765a80c25edf61d7284efbadbec976ad
Baru <lukasz.bar@wp.pl>
2013-12-28 10:10:08 +0100

    dzialajaca wersja

--- ruby.rb
+++ ruby.rb
@@ -6,17 +6,20 @@ while choose != 4 do
 	if choose == 1
 		puts "What is the Album name?"
 		album_name=gets.chomp
-		names.push(album_name.capitalize)
-		puts "Album #{album_name} was added to you collection"
-	elsif choose == 2#asddda
+		if !names.include?(album_name.capitalize)
+			names.push(album_name.capitalize)
+			puts "Album #{album_name} was added to you collection"
+		else
+			puts "Choose another name"
+		end
+	elsif choose == 2
 		puts "Which album do you want to delete?"
 		album_del= gets.chomp
-		if names.include?(album_del)
+		if !names.include?(album_del.capitalize)
 			puts "Can't find this album"
-		else
-		names.delete_if {|name| name.downcase==album_del}
-		#	puts "Album #{album_del} deleted"
-		 #puts "Can't find this album"
+		else	
+		names.delete_if {|name| name==album_del.capitalize}
+		puts "Album #{album_del} deleted"
 		end
 	else choose == 3
 		puts "Your albums:"
