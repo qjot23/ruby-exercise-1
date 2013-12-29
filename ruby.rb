392bb944907d47557830b2af5351737858e8ab16
Baru <lukasz.bar@wp.pl>
2013-12-29 13:20:55 +0100

    after refactoring

--- ruby.rb
+++ ruby.rb
@@ -1,12 +1,12 @@
 names = Array.new
 choose=0
-while choose != 4 do
+loop do
 	puts "What do you want to do?\n1. Add album\n2. Remove Album\n3. Show Album\n4. Quit"
 	choose = gets.to_i
 	if choose == 1
 		puts "What is the Album name?"
 		album_name=gets.chomp
-		if !names.include?(album_name.capitalize)
+		unless names.include?(album_name.capitalize)
 			names.push(album_name.capitalize)
 			puts "Album #{album_name} was added to you collection"
 		else
@@ -15,19 +15,17 @@ while choose != 4 do
 	elsif choose == 2
 		puts "Which album do you want to delete?"
 		album_del= gets.chomp
-		if !names.include?(album_del.capitalize)
+		unless names.include?(album_del.capitalize)
 			puts "Can't find this album"
 		else	
-		names.delete_if {|name| name==album_del.capitalize}
-		puts "Album #{album_del} deleted"
+			names.delete(album_del.capitalize)
+			puts "Album #{album_del} deleted"
 		end
-	else choose == 3
+	elsif choose == 3
 		puts "Your albums:"
-		names.each do |i| puts i
+		names.each {|i| puts i}
+	else
+		puts "Choose between 1-4"
+		break if choose==4
 	end
-end	
-if choose == 4
-	abort
 end
-end
-koniec = gets
